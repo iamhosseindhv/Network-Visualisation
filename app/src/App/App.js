5f718d4849d58bf092417422305e6e15b55e4db9
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-04-29 23:11:10 +0100

    added infrastructure to get data using api call

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import purple from 'material-ui/colors/purple';
 import green from 'material-ui/colors/green';
-import createPalette from 'material-ui/styles/palette';
-
 import D3Graph from '../D3Graph/D3Graph';
 import Layout from '../Layout/Layout';
 
+import data from '../data';
 
 
 const muiTheme = createMuiTheme({
@@ -27,11 +26,27 @@ const muiTheme = createMuiTheme({
 export default class App extends Component {
 	constructor(properties) {
 		super(properties)
+		this.state = {
+			graphData: {},
+			graphConfig: {},
+		}
+
+
+		this.getGraphData()
+			.then(data => this.setState({ graphData: data }))
+			.catch(err => console.log(err));
 	}
 
-	state = {
-		graphConfig: {},
-	}
+	getGraphData = async () => {
+		// const response = await fetch('/data/graph_name');
+		// const body = await response.json();
+		// if (response.status !== 200) throw Error(body.message);
+		// return body;
+
+		//fake async
+		return data;
+	};
+
 
 	onChangeForm = data => { this.setState({ graphConfig: data }) }
 
@@ -39,7 +54,8 @@ export default class App extends Component {
 		return (
 			<MuiThemeProvider theme={muiTheme}>
 				<Layout onChangeForm={this.onChangeForm}>
-					<D3Graph config={this.state.graphConfig} />
+					<D3Graph data={this.state.graphData} config={this.state.graphConfig} />
+					{/* <D3Graph config={this.state.graphConfig} /> */}
 				</Layout>
 			</MuiThemeProvider>
 		);
