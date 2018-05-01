73e35370d49f43e723c8107b717306dc0a3f8f5a
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-05-01 23:46:44 +0100

    tested connecting to db and returning results

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/hello', (req, res) => {
-  res.send({ express: 'Hello From Expressss' });
+
+  var getConnection = require('../database');
+  getConnection((err, connection) => {
+    if (err) throw err; // for now we just throw
+
+    connection.query('SELECT * FROM listing', function (error, result) {
+      if (error) throw error;
+      connection.release();
+      res.send({
+        express: 'Hello From Expressss',
+        result: result[0],
+      });
+    });
+  });
+
 });
 
 module.exports = router;
