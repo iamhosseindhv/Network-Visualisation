401a839ffa0f411e7a6902ab5bf2b3b0fac776c3
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-05-01 23:56:00 +0100

    added support for dotenv variables

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
+
 
 var apiRouter = require('./routes/api');
 
 var app = express();
+dotenv.config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
