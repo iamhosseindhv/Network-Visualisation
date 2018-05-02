1ebd3010d9e88d9408c2dc9ca289f5b6a2a25cc7
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-05-02 23:38:43 +0100

    removed rendering a view when error occurs in Development env

--- server/app.js
+++ server/app.js
@@ -37,7 +37,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
+  // res.render('error');
 });
 
 module.exports = app;
