88407eb3453a159ba277fa3dfc353d5223559b58
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-05-04 17:58:39 +0100

    converted tabs to 4space and corrected formatting

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -6,41 +6,41 @@ import D3Graph from '../D3Graph/D3Graph';
 import Layout from '../Layout/Layout';
 
 const muiTheme = createMuiTheme({
-	palette: {
-		primary: {
-			light: purple[300],
-			main: purple[500],
-			dark: purple[700],
-		},
-		secondary: {
-			light: green[300],
-			main: green[500],
-			dark: green[700],
-		},
-	},
+    palette: {
+        primary: {
+            light: purple[300],
+            main: purple[500],
+            dark: purple[700],
+        },
+        secondary: {
+            light: green[300],
+            main: green[500],
+            dark: green[700],
+        },
+    },
 });
 
 export default class App extends Component {
-	constructor(properties) {
-		super(properties)
-		this.state = {
-			graphData: {},
-			graphConfig: {},
-		};
-	}
+    constructor(properties) {
+        super(properties)
+        this.state = {
+            graphData: {},
+            graphConfig: {},
+        };
+    }
 
-	onChangeForm = config => { this.setState({ graphConfig: config }) };
-	
-	onChangeData = data => { this.setState({ graphData: data }) };
+    onChangeForm = config => { this.setState({ graphConfig: config }) };
 
-	render() {
-		return (
-			<MuiThemeProvider theme={muiTheme}>
-				<Layout onChangeForm={this.onChangeForm} onChangeData={this.onChangeData}>
-					<D3Graph data={this.state.graphData} config={this.state.graphConfig} />
-				</Layout>
-			</MuiThemeProvider>
-		);
-	}
+    onChangeData = data => { this.setState({ graphData: data }) };
+
+    render() {
+        return (
+            <MuiThemeProvider theme={muiTheme}>
+                <Layout onChangeForm={this.onChangeForm} onChangeData={this.onChangeData}>
+                    <D3Graph data={this.state.graphData} config={this.state.graphConfig} />
+                </Layout>
+            </MuiThemeProvider>
+        );
+    }
 
 }
--- server/database.js
+++ server/database.js
@@ -4,11 +4,11 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-    connectionLimit : 20,
+    connectionLimit: 20,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE,
 });
 
 const getConnection = callback => {
