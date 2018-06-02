8834d4e9fd5add96839098831718d39cb9553dc7
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-06-02 14:47:20 +0100

    added transition for inputs based on drawer position

--- app/src/Filters/Filters.js
+++ app/src/Filters/Filters.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -12,9 +13,26 @@ import Utils from '../utils/communications';
 
 
 const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
     formControl: {
-        // margin: theme.spacing.unit,
-        minWidth: '100% !important',
+        minWidth: 232,
+    },
+    formControlMoveRight: {
+        transform: 'translate3d(' + theme.spacing.unit * 6 + 'px, 0, 0)',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.leavingScreen,
+            easing: theme.transitions.easing.sharp,
+        }),
+    },
+    formControlMoveLeft: {
+        transform: 'translate3d(0, 0, 0)',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.leavingScreen,
+            easing: theme.transitions.easing.sharp,
+        }),
     },
 });
 
@@ -27,12 +45,15 @@ class Filters extends Component {
         };
     };
 
-    handleGraphChange = event => {
-        const currentGraph = event.target.value;
-        const selectedGraph = this.state.availableGraphs.find(graph => graph.name === currentGraph);
-        this.setState({ currentGraph });
-        Utils.getGraphData(selectedGraph.id)
-            .then(this.updateData)
+    handleGraphChange = () => {
+        const selectedGraph = this.state.availableGraphs.find(graph => graph.name === this.state.currentGraph);
+        //modify this
+        const query = {
+            id: selectedGraph.id,
+            ...this.state,
+        };
+        Utils.getGraphData(selectedGraph.id, query)
+            .then(graphData => this.props.onChangeData(graphData))
             .catch(this.handleError);
     };
 
@@ -42,34 +63,40 @@ class Filters extends Component {
         console.log(err);
     };
 
-    handleChange = event => { this.setState({ [event.target.name]: event.target.value }) };
-
-    updateData = graphData => { this.props.onChangeData(graphData) };
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value }, () => {
+            this.handleGraphChange();
+        })
+    };
 
     componentDidMount = () => {
         Utils.getAvailableGraphs()
             .then(data => this.setState({ availableGraphs: data }))
             .catch(this.handleError);
-    }
+    };
 
     render() {
-        const { classes } = this.props;
+        const { classes, open } = this.props;
+        const formControlClassName = classNames(classes.formControl, open ? classes.formControlMoveLeft : classes.formControlMoveRight);
         return (
             <Fragment>
-                <Subheader id="graph-search" title="Search" icon={<SearchIcon />}/>
-                <ListItem >
-                    <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor="graph-id">Choose your graph</InputLabel>
-                        <Select
-                            value={this.state.currentGraph}
-                            onChange={this.handleGraphChange}
-                            inputProps={{ name: 'currentGraph', id: 'graph-id' }}
-                        >
-                            {this.state.availableGraphs.map(graph => {
-                                return <MenuItem key={graph.id} value={graph.name}>{graph.name}</MenuItem>
-                            })}
-                        </Select>
-                    </FormControl>
+                <Subheader id="graph-search" title="Search" icon={<SearchIcon />} />
+                <ListItem>
+                    <form className={classes.root}>
+                        <FormControl className={formControlClassName}>
+                            <InputLabel htmlFor="graph-id">Choose your graph</InputLabel>
+                            <Select
+                                value={this.state.currentGraph}
+                                onChange={this.handleChange}
+                                inputProps={{ name: "currentGraph", id: "graph-id" }}
+                            >
+                                {this.state.availableGraphs.map(graph => {
+                                    return <MenuItem key={graph.id} value={graph.name}>{graph.name}</MenuItem>
+                                })}
+                            </Select>
+                        </FormControl>
+                    
+                    </form>
                 </ListItem >
             </Fragment>
         );
@@ -85,6 +112,11 @@ Filters.propTypes = {
      * @ignore
      */
     theme: PropTypes.object.isRequired,
+    /**
+     * Whether the left drawer is open or not.
+     * Needed for transition of input elements
+     */
+    open: PropTypes.bool.isRequired,
     /**
      * @param {object} event The event source of the callback
      */
