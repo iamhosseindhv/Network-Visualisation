6f913fd1b06a6d68b40e98cbe66c002382c3c229
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2018-06-02 17:11:53 +0100

    modified changeHandler, added age-gender inputs

--- app/src/Filters/Filters.js
+++ app/src/Filters/Filters.js
@@ -6,40 +6,44 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import ListItem from '@material-ui/core/ListItem';
+import FormGroup from '@material-ui/core/FormGroup';
 import FormControl from '@material-ui/core/FormControl';
 import SearchIcon from '@material-ui/icons/Search';
 import Subheader from '../Subheader/Subheader';
 import Utils from '../utils/communications';
 import styles from './Filters.styles';
 
-/* full version of a state
-this.state = {
-    currentGraph: 'test',
-    availableGraphs: [{name: 'test', graph_id:'10'}, {name: 'test2', graph_id:'20'}],
-    infoGender: 'Male',
-    infoLocation: 'Tehran',
-    infoAge: '25-30',
-    infoOccupation: ['skill0', 'skill1', 'skill2'],
-};
-*/
+const genderDatasource = ['Male', 'Female'].map((gender, i) => {
+    return <MenuItem key={i} value={gender.toLowerCase()}>{gender}</MenuItem>
+});
+
+const ageDatasource = ['20-25', '25-30', '30-40', '40-50', '50-60', '60-100'].map((age, i) => {
+    return <MenuItem key={i} value={age}>{age}</MenuItem>
+});
 
 class Filters extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentGraph: '',
             availableGraphs: [],
+            currentGraph: '',
+            disableFilters: true,
+            graphData: {
+                gender: '',
+                location: '',
+                age_range: '',
+                occupation: [],
+            },
         };
     };
 
-    handleGraphChange = () => {
+    updateGraphData = () => {
         const selectedGraph = this.state.availableGraphs.find(graph => graph.name === this.state.currentGraph);
-        //modify this
         const query = {
-            id: selectedGraph.id,
-            ...this.state,
+            graph_id: selectedGraph.id,
+            ...this.state.graphData,
         };
-        Utils.getGraphData(selectedGraph.id, query)
+        Utils.getGraphData(query)
             .then(graphData => this.props.onChangeData(graphData))
             .catch(this.handleError);
     };
@@ -51,9 +55,17 @@ class Filters extends Component {
     };
 
     handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value }, () => {
-            this.handleGraphChange();
-        })
+        const isGraphChange = event.target.name === 'currentGraph';
+        var newState = Object.assign({}, this.state);
+        if (isGraphChange) {
+            newState.currentGraph = event.target.value;
+            newState.disableFilters = false;
+        } else {
+            newState.graphData[event.target.name] = event.target.value;
+        }
+        this.setState(newState, () => {
+            this.updateGraphData();
+        });
     };
 
     componentDidMount = () => {
@@ -70,29 +82,40 @@ class Filters extends Component {
                 <Subheader id="graph-search" title="Search" icon={<SearchIcon />} />
                 <ListItem>
                     <form className={classes.root}>
-                        <FormControl className={formControlClassName}>
-                            <InputLabel htmlFor="graph-id">Choose your graph</InputLabel>
+                        <FormControl fullWidth="true" className={formControlClassName}>
+                            <InputLabel htmlFor="currentGraph">Choose your graph</InputLabel>
                             <Select
                                 value={this.state.currentGraph}
                                 onChange={this.handleChange}
-                                inputProps={{ name: "currentGraph", id: "graph-id" }}
+                                inputProps={{ name: "currentGraph", id: "currentGraph" }}
                             >
                                 {this.state.availableGraphs.map(graph => {
                                     return <MenuItem key={graph.id} value={graph.name}>{graph.name}</MenuItem>
                                 })}
                             </Select>
                         </FormControl>
-                        <FormControl className={formControlClassName}>
-                            <InputLabel htmlFor="info-gender">Gender</InputLabel>
-                            <Select
-                                value={this.state.infoGender}
-                                onChange={this.handleChange}
-                                inputProps={{ name: "infoGender", id: "info-gender" }}
-                            >
-                                <MenuItem key={1} value="Male">Male</MenuItem>
-                                <MenuItem key={2} value="Female">Female</MenuItem>
-                            </Select>
-                        </FormControl>
+                        <FormGroup row>
+                            <FormControl className={classNames(formControlClassName, classes.formControlLeft)} disabled={this.state.disableFilters}>
+                                <InputLabel htmlFor="gender">Gender</InputLabel>
+                                <Select
+                                    value={this.state.graphData.gender}
+                                    onChange={this.handleChange}
+                                    inputProps={{ name: "gender", id: "gender" }}
+                                >
+                                    {genderDatasource}
+                                </Select>
+                            </FormControl>
+                            <FormControl className={classNames(formControlClassName, classes.formControlRight)} disabled={this.state.disableFilters}>
+                                <InputLabel htmlFor="age_range">Age</InputLabel>
+                                <Select
+                                    value={this.state.graphData.age_range}
+                                    onChange={this.handleChange}
+                                    inputProps={{ name: "age_range", id: "age_range" }}
+                                >
+                                    {ageDatasource}
+                                </Select>
+                            </FormControl>
+                        </FormGroup>
                     </form>
                 </ListItem >
             </Fragment>
